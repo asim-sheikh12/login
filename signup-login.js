69b3e07e02dc9ba933cdebb52fc757479694bc4d
Asim <asim.thoughtwin@gmail.com>
2022-01-18 17:21:49 +0530

    initial commit

--- signup-login.js
+++ signup-login.js
@@ -1,72 +1,115 @@
-var signUp = e =>
-{
-    let formData = 
-    {
-        firstname: document.getElementById('firstname').value,
-        lastname: document.getElementById('lastname').value,
-        email: document.getElementById('email').value,
-        date: document.getElementById('date').value,
-        pw1: document.getElementById('pass1').value,
-        pw2: document.getElementById('pass2').value,
-        
-    }
-   
-   localStorage.setItem('formData',JSON.stringify(formData));
-    JSON.parse(localStorage.getItem('formData'))
-    console.log(localStorage.getItem('formData'))
+//Sign Up
+
+function signup() {
+  let pass1 = document.getElementById("pass1").value;
+  let pass2 = document.getElementById("pass2").value;
+  if (pass1 != pass2) {
+    alert("Password does not match");
+  } //Sign Up
+
+  var signUp = (e) => {
+    let formData = {
+      firstname: document.getElementById("firstname").value,
+      lastname: document.getElementById("lastname").value,
+      email: document.getElementById("email").value,
+      date: document.getElementById("date").value,
+      pw1: document.getElementById("pass1").value,
+      pw2: document.getElementById("pass2").value,
+    };
+
+    localStorage.setItem("formData", JSON.stringify(formData));
+    JSON.parse(localStorage.getItem("formData"));
+    console.log(localStorage.getItem("formData"));
     e.preventDefault();
     console.log(formData);
 
-{
-let pass1 = document.getElementById('pass1').value
-let pass2 = document.getElementById('pass2').value
-if(pass1!=pass2)
-{
-alert("Password does not match")
-}
-else
-{
-alert(`Hi ${formData.firstname}! You have signed up successfully.`)
-}
-}
-}
+    {
+      let pass1 = document.getElementById("pass1").value;
+      let pass2 = document.getElementById("pass2").value;
+      if (pass1 != pass2) {
+        alert("Password does not match");
+      } else {
+        alert(`Hi ${formData.firstname}! You have signed up successfully.`);
+      }
+    }
+  };
 
+  //Login page:-
+
+  function signupPage() {
+    window.open("https://asim-thoughtwin.github.io/signup/");
+  }
+
+  const login = (e) => {
+    let loginData = {
+      email: document.getElementById("email").value,
+      pw2: document.getElementById("pass2").value,
+    };
+    localStorage.setItem("loginData", JSON.stringify(loginData));
+    console.log(localStorage.getItem("loginData"));
+    e.preventDefault();
+
+    console.log(loginData);
+
+    const registerData = JSON.parse(localStorage.getItem("formData"));
+
+    if (
+      registerData.email === loginData.email &&
+      registerData.pw2 === loginData.pw2
+    ) {
+      alert("User is Aunthenticated");
+    } else {
+      alert("Either Password or email is Incorrect");
+    }
+  };
+}
+const signUp = (e) => {
+  let formData = {
+    firstname: document.getElementById("firstname").value,
+    lastname: document.getElementById("lastname").value,
+    email: document.getElementById("email").value,
+    date: document.getElementById("date").value,
+    pw1: document.getElementById("pass1").value,
+    pw2: document.getElementById("pass2").value,
+  };
+
+  localStorage.setItem("formData", JSON.stringify(formData));
+  JSON.parse(localStorage.getItem("formData"));
+  console.log(localStorage.getItem("formData"));
+  e.preventDefault();
+  console.log(formData);
+};
 //Login page:-
 
 function signupPage() {
   window.open("https://asim-thoughtwin.github.io/signup/");
 }
 
-const login = (e) =>
-{
-    let loginData = 
-    {
-        email: document.getElementById('email').value,
-        pw2: document.getElementById('pass2').value,
-        
+const login = (e) => {
+  let loginData = {
+    email: document.getElementById("email").value,
+    pw2: document.getElementById("pass2").value,
+  };
+  console.log(localStorage.getItem("loginData"));
+  e.preventDefault();
+
+  console.log(loginData);
+
+  const registerData = JSON.parse(localStorage.getItem("formData"));
+
+  if (
+    registerData.email === loginData.email &&
+    registerData.pw2 === loginData.pw2
+  ) {
+    alert("User is Aunthenticated");
+  } else {
+    if (registerData.email !== loginData.email) {
+      alert("Email is Incorrect");
+    } else {
+        if(registerData.pw2!==loginData.pw2)
+        {
+            alert('Password is incorrect!')
+        }
     }
-    localStorage.setItem('loginData',JSON.stringify(loginData));
-    console.log(localStorage.getItem('loginData'))
-    e.preventDefault();
-
-    console.log(loginData);
-
-    const registerData =  JSON.parse(localStorage.getItem('formData'))
-
-      if(registerData.email===loginData.email && registerData.pw2===loginData.pw2)
-      {
-          alert("User is Aunthenticated")
-
-      }
-
-else{
-    alert("Either Password or email is Incorrect")
-}
-    
-}
-
-
-
-
-
-
+  }
+};
